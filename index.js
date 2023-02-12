4ebce1bb5e2003b8caa10f700abbcaea2a184771
Shelby Morris <shelbynicole.morris@gmail.com>
2023-02-11 18:21:20 -0700

    completed test with help from TC Connor Holland

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+  const arr = [];
+    for (let i = 0; i < names.length; i++) {
+      arr.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    };
+  return arr;
+  };
+  
+  
+  function countDown() {
+    let int = 10;
+    while (int >= 0) {
+      console.log(int--)
+    };
+  };
